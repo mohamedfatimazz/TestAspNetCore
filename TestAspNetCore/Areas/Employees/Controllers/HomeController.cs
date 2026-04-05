6be162093e8d81ea1c330dbf1877ae1bf39e5bea
mohamedfatimazz <aljowadfa@gmail.com>
2026-04-05 20:46:32 +0300

    .

--- TestAspNetCore/Areas/Employees/Controllers/HomeController.cs
+++ TestAspNetCore/Areas/Employees/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TestAspNetCore.Areas.Employees.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         [Area("Employees")]
